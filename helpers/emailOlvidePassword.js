2c59a0dfc40f8328670307ed17f1ee6cb80cfc0c
Daniel Leon <danielmatus468@gmail.com>
2022-10-12 17:11:30 -0500

    fixed email olvide password

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -1,28 +1,24 @@
-import nodemailer from "nodemailer"
+import sgMail from '@sendgrid/mail'
 
 const emailolvidePassword = async (datos) => {
-    const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const { email, nombre, token } = datos
-    const info = await transport.sendMail({
-        from: 'APV - Administrador de Pacientes de Veterinaria',
+    const msg = {
         to: email,
-        subject: 'Reestablece tu Password',
-        text: 'Reestablece tu Password',
+        from: 'matusleonc@outlook.com',
+        subject: 'Comprueba tu cuenta en APV',
         html: `
-            <p>Hola ${nombre}, has solicitado reestablecer tu password </p>
-            <p> Sigue el siguiente enlace para generar un nuevo password: 
-            <a href='${process.env.FRONTEND_URL}/olvide-password/${token}'>Reestablecer Password </a> </p>
-            <p> Si tu no pediste reestablecer tu contraseña, puedes ignorar este mensaje </p>
+            <p>Hola ${nombre}, comprueba tu cuenta en APV </p>
+            <p> Tu cuenta ya esta lista, solo debes comprobarla en el siguiente enlace: 
+            <a href='${process.env.FRONTEND_URL}/confirmar/${token}'>Comprobar Cuenta </a> </p>
+            <p> Si tu no creaste esta cuenta, puedes ignorar este mensaje </p>
         `
-    })
-    console.log("Mensaje enviado: %s", info.messageId)
+
+    }
+
+    sgMail.send(msg)
+        .then(res => console.log(res))
+        .catch(error => console.log(error.message))
 }
 
 export default emailolvidePassword
