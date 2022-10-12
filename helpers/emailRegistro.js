dd9cd57188b5f8240a3777357f714dc310af551a
Daniel Leon <danielmatus468@gmail.com>
2022-10-12 17:07:27 -0500

    added sengrid to project

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -1,28 +1,24 @@
-import nodemailer from "nodemailer"
+import sgMail from '@sendgrid/mail'
 
 const emailRegistro = async (datos) => {
-    const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const { email, nombre, token } = datos
-    const info = await transport.sendMail({
-        from: 'APV - Administrador de Pacientes de Veterinaria',
+    const msg = {
         to: email,
+        from: 'matusleonc@outlook.com',
         subject: 'Comprueba tu cuenta en APV',
-        text: 'Comprueba tu cuenta en APV',
         html: `
             <p>Hola ${nombre}, comprueba tu cuenta en APV </p>
             <p> Tu cuenta ya esta lista, solo debes comprobarla en el siguiente enlace: 
             <a href='${process.env.FRONTEND_URL}/confirmar/${token}'>Comprobar Cuenta </a> </p>
             <p> Si tu no creaste esta cuenta, puedes ignorar este mensaje </p>
         `
-    })
-    console.log("Mensaje enviado: %s", info.messageId)
+
+    }
+
+    sgMail.send(msg)
+        .then(res => console.log(res))
+        .catch(error => console.log(error.message))
 }
 
 export default emailRegistro
